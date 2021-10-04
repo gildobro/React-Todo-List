6d57e3e93afc307786f5e7b6c61bc5fd355522d6
gildobro <gdobrovinsky@gmail.com>
2021-10-04 11:23:04 -0400

    add todo item

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useReducer, useRef } from 'react';
 import { render } from 'react-dom';
 
 import './style.css';
 import * as constants from './constants';
 
 //const initialState = [...constants.TODOS];
-const initialState = [
-  {id: 1, name: 'Get started', complete: false}
-];
+const initialState = [{ id: 1, name: 'Get started', complete: false }];
+
+//set up a function that takes state and action, then switch on action.type
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      return action.name.length
+        ? [
+            ...state,
+            {
+              id: state.length
+                ? Math.max(...state.map((todo) => todo.id)) + 1
+                : 0,
+              name: action.name,
+              complete: false,
+            },
+          ]
+        : state;
+    }
+  }
+};
 
 const Todo = () => {
+  const inputRef = useRef();
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  function addTodo(event) {
+    event.preventDefault();//Prevents the page from refreshing when hitting submit
+    dispatch({
+      type: 'ADD_TODO',
+      name: inputRef.current.value,
+      complete: false,
+    });
+    inputRef.current.value = '';
+  }
   return (
     <>
       <div className="todo-input">
-        <form>
-          <input type="search" id="add-todo" placeholder="Add Todo..." />
+        <form onSubmit={addTodo}>
+          <input
+            ref={inputRef}
+            type="search"
+            id="add-todo"
+            placeholder="Add Todo..."
+          />
         </form>
       </div>
       <div className="column-container">
-        {initialState.map((todo) => (
+        {todos.map((todo) => (
           <div key={todo.id} alt={todo.id} className="column-item">
             <div className="flex-container">
               <div className="todo-name">{todo.name}</div>
@@ -27,11 +61,9 @@ const Todo = () => {
           </div>
         ))}
       </div>
-      <button>
-        CLEAR TODOS
-      </button>
+      <button>CLEAR TODOS</button>
     </>
   );
-}
+};
 
 render(<Todo />, document.getElementById('root'));
